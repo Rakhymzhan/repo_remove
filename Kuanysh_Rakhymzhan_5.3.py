577688d6a769e452d1eddfeed8a854b914592bab
Rakhymzhan <92759524+Rakhymzhan@users.noreply.github.com>
2021-11-16 21:54:08 +0600

    Add files via upload

---
+++ Kuanysh_Rakhymzhan_5.3.py
@@ -0,0 +1,20 @@
+tutors = ['Иван', 'Анастасия', 'Игорь', 'Роберт', 'Сергей', 'Егор', 'Император']
+
+
+klasses = ['11a', '10c', '9l', '7c', '1q', '777']
+
+if len(tutors) > len(klasses):
+
+
+    klasses += [None for i in range(len(tutors) - len(klasses))]
+
+result = ((tutors[i], klasses[i]) for i in range(len(tutors)))
+
+
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
