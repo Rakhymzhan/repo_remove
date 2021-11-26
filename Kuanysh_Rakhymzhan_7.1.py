87f7ce7f4762a425ea48405134ec2f8055c71ac9
Rakhymzhan <92759524+Rakhymzhan@users.noreply.github.com>
2021-11-26 11:54:03 +0600

    Delete Kuanysh_Rakhymzhan_7.1.py

--- Kuanysh_Rakhymzhan_7.1.py
+++
@@ -1,9 +0,0 @@
-import os
-
-ROOT = os.path.dirname(__file__)
-project_name = 'my_project'
-paths = [os.path.join(project_name, 'settings'), os.path.join(project_name, 'mainapp'),
-         os.path.join(project_name, 'adminapp'),
-         os.path.join(project_name, 'authapp')]
-for path in paths:
-    os.makedirs(os.path.join(ROOT, _path), exist_ok=True)
